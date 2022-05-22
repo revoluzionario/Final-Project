cf496bf17829db493ea89b2cbc03b01cec47244e
Lê Công Hoàng - 21020322 <94961228+revoluzionario@users.noreply.github.com>
2022-05-22 15:13:42 +0700

    Update main.cpp

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -29,7 +29,7 @@ int main (int argc, char* argv[])
             {
                 if (music)
                 {
-                    Mix_PlayMusic(backgroundMusic, 0);
+                    Mix_PlayMusic(backgroundMusic, -1);
                 }
                 switch (state)
                 {
